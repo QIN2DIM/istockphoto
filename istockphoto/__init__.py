b4ae29d1f9666291b9cf8f5097f84dfbdc94f1bc
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2023-09-12 11:10:45 +0800

    update: from gevent to asyncio

--- istockphoto/__init__.py
+++ istockphoto/__init__.py
@@ -3,7 +3,3 @@
 # Author     : QIN2DIM
 # Github     : https://github.com/QIN2DIM
 # Description:
-from .core import IstockPhotoDownloader, MediaType, Orientations, NumberOfPeople
-
-__all__ = ["IstockPhotoDownloader", "MediaType", "Orientations", "NumberOfPeople"]
-__version__ = "0.0.3"
